51b1dce9c1aae0bb149ab09a2583e40441daf9f4
ADMIN <isaacvwarui@gmail.com>
2025-02-28 14:37:00 +0300

    Ui

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/nav.css";
 
 function Nav() {
   return (
-    <nav className="">
-      <ul className="flex justify-between items-center">
+    <nav className="nav-container">
+      <ul className="nav-list">
         {/* Left Side (Logo) */}
         <li>
-          <Link to="/" className="no-underline text-inherit font-normal">
+          <Link to="/" className="nav-logo">
             Isaac.
           </Link>
         </li>
 
         {/* Right Side (Navigation Links) */}
-        <div className="">
+        <div className="nav-links">
           <li>
-            <Link to="/experiance" className="">
+            <Link to="/experiance" className="nav-link">
               Experience
             </Link>
           </li>
           <li>
-            <Link to="/projects" className="">
+            <Link to="/projects" className="nav-link">
               Projects
             </Link>
           </li>
           <li>
-            <Link to="/certifications" className="">
+            <Link to="/certifications" className="nav-link">
               Certifications
             </Link>
           </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,31 +6,34 @@ function Home() {
   return (
     <>
       <Nav />
-      <div>
-        <div>
-          <h2 className="">Hello, I am a Software Developer</h2>
-          <h1 className="">Isaac Victor Warui</h1>
+      <div className="container">
+        <div className="box">
+          <div style={{}}>
+            <p className="" style={{ fontSize: "50px" }}>Hello, I am a Software Developer</p>
+            <h1 className="" style={{ fontSize: "100px" }}>Isaac <span style={{ color: "#666" }}>Warui</span></h1>
+          </div>
+          <div className="" style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "300px", height: "300px" }}>
+            <h1>Image Here</h1>
+          </div>
         </div>
-        <div>
-          <h1>Image Here</h1>
-        </div>
-      </div>
-      <div>
-        <div>
-          <h2>1</h2>
-          <p>Experience</p>
-        </div>
-        <div>
-          <h2>5</h2>
-          <p>Projects</p>
-        </div>
-        <div>
-          <h2>2</h2>
-          <p>Certifications</p>
-        </div>
-        <div>
-          <h2>2</h2>
-          <p>Languages</p>
+        <div className="box home_footer">
+          <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <h2>1</h2>
+            <p>Experience</p>
+          </div>
+          <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <h2>5</h2>
+            <p>Projects</p>
+          </div>
+          <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <h2>2</h2>
+            <p>Certifications</p>
+          </div>
+          <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
+            <h2>1</h2>
+            <p>Languages</p>
+          </div>
+                    
         </div>
       </div>
     </>
