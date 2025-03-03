d645cfae934048aaf9b842cc30adae131e4c6bac
ADMIN <isaacvwarui@gmail.com>
2025-03-03 14:27:28 +0300

    proj edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 // import "./App.css";
 import Home from "./pages/Home";
 import Cert from "./pages/Cert";
